04464589302b2cd1e399347e666df3c0616c7054
strongme <624917867@qq.com>
2012-12-09 00:10:38 +0800

    new add Weibo

---
+++ Weibo/vml/vml1.js
@@ -0,0 +1,12 @@
+/**
+ * @author walter
+ */
+$(function() {
+	$("#circle").mouseover(function() {
+		$(this).css("width",600);
+	}).mouseout(function() {
+		$(this).css("width",150);
+	});
+	
+	
+});
